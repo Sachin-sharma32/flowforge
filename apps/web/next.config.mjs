4b370d978ec46755f6844945a35a0bed9fbf7f0b
Sachin-sharma32 <sachin2sharma001@gmail.com>
2026-04-06 15:42:09 +0000

    feat: add code-inspector-plugin for convenient code analysis and make inspections faster and easier

--- apps/web/next.config.mjs
+++ apps/web/next.config.mjs
@@ -1,7 +1,11 @@
-
+import { codeInspectorPlugin } from 'code-inspector-plugin';
 
 const nextConfig = {
   transpilePackages: ['@flowforge/shared'],
+  webpack: (config, { dev, isServer }) => {
+    config.plugins.push(codeInspectorPlugin({ bundler: 'webpack' }));
+    return config;
+  },
 };
 
 export default nextConfig;
